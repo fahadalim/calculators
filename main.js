9dbe2443071e8596f0cf08663fb29a8c14340a9f
fahadalim <mohammadfahadalim@gmail.com>
2022-03-05 01:23:35 +0530

    cal

---
+++ main.js
@@ -0,0 +1,9 @@
+const add = require("./add")
+const subtract = require("./subtract")
+const multiply = require("./multiply")
+const divide = require("./divide")
+
+add(3,7)
+subtract(6,7)
+multiply(9,7)
+divide(2,4)
